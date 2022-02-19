8a94867d7a37c1808cea437c9302b92bf7332f17
vijaya r challa <challa_naidu@hotmail.com>
2022-02-18 18:49:07 -0600

    Created the basicproject structure #1

---
+++ DotBasics.Test/ExcelTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DotBasics.Test
+{
+    public class ExcelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ DotBasics/Excel/Exporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotBasics.Excel
+{
+
+    public class Exporter<T> where T : ICollection<T>
+    {
+        private ICollection<T> _iCollection;
+        public Exporter(ICollection<T> iCollection) { 
+            _iCollection = iCollection;
+        }
+    }
+    
+}
